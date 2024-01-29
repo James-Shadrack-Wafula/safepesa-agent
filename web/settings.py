5cfb95b52fe1ddc6114c2078f55cfd22a8a43832
James-Shadrack-Wafula <jamesshadrackwafula.gmail.com>
2024-01-29 17:26:52 +0300

    using allowed hosts *

--- web/settings.py
+++ web/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # DEBUG = True
 DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 APPEND_SLASH=False
 # Application definition
