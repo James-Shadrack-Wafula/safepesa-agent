121987e3f2012b2fb290455867315d037b0c438e
James-Shadrack-Wafula <jamesshadrackwafula.gmail.com>
2024-01-30 01:25:00 +0300

    configure static

--- web/urls.py
+++ web/urls.py
@@ -18,10 +18,13 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
