f68cb99ff0fc7cbc3beee573b8a67429d5d6582e
James-Shadrack-Wafula <jamesshadrackwafula.gmail.com>
2024-01-31 00:22:24 +0300

    collected static

--- web/settings.py
+++ web/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # 'rest_framework.authtoken.middleware.TokenMiddleware',
     
     
